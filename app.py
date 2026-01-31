88341b7500c1ce67c53f9f741acea7f016aa591e
AH-SING <mathieu.ahsing@gmail.com>
2026-01-31 22:39:54 +0100

    punchline réalisées pour les mentors

--- app.py
+++ app.py
@@ -19,20 +19,20 @@ for i, m in enumerate(MENTORS):
             state_manager.set_coach(m)
             st.rerun()
 
-ui_components.step_title(1, "Comment t'apelles-tu disciple ?")
+titre_nom = st.session_state.coach['punchlines'].get('nom', "Comment t'appelles-tu ?")
+ui_components.step_title(1, titre_nom)
 prenom_raw = st.text_input("Surnom", placeholder="Garry", label_visibility="collapsed")
 prenom = "".join(x for x in prenom_raw if x.isalnum())[:20] or "Ami"
 
-# Dans app.py
-ui_components.step_title(2, "Dis moi ton rang sur l'échiquier")
+titre_elo = st.session_state.coach['punchlines'].get('elo', "elo ?")
+ui_components.step_title(2, titre_elo)
 user_elo = ui_components.elo_selector()
 
 # Lors de la création du JSON pour l'IA :
 #payload["players"]["user"]["elo_rating"] = user_elo
 
-
-
-ui_components.step_title(3, "Ta partie")
+titre_partie = st.session_state.coach['punchlines'].get('pgn', "pgn ?")
+ui_components.step_title(3, titre_partie)
 label_couleur = "⚪ BLANCS" if st.session_state.joueur_est_blanc else "⚫ NOIRS"
 st.toggle(label_couleur, key="joueur_est_blanc") # Streamlit gère le lien auto avec la clé
 
@@ -62,8 +62,7 @@ actions = {
     "boa": f"{st.session_state.coach['nom']}, montre-moi où j'ai serré..."
 }
 
-# On récupère le texte, avec un fallback sécurisé
-button_label = actions.get(st.session_state.coach['id'], "🔍 Analyser la partie")
+button_label =st.session_state.coach['punchlines'].get('analyse', "analyse ?")
 
 # 4. ACTION
 if st.button(button_label, type="primary", use_container_width=True):
--- constants.py
+++ constants.py
@@ -4,11 +4,12 @@ MENTORS = [
         "nom": "Le prof de Club",
         "emoji": "👨‍🏫",
         "desc": "Le gardien des principes. Il ne tolère aucune entorse aux bases fondamentales.",
-        "vibe": "Rigueur, structure et pédagogie."
+        "vibe": "Rigueur, structure et pédagogie.",
         "punchlines": {
             "nom": "Rappelle-moi ton nom, qu'on ressorte ton dossier.",
             "elo": "On en est où de ton classement ? Toujours en progression, j'espère.",
-            "pgn": "Montre-moi ta dernière partie. Voyons si les leçons ont été retenues."
+            "pgn": "Montre-moi ta dernière partie. Voyons si les leçons ont été retenues.",
+            "analyse": "Analysons cette partie"
         }
     },
     {
@@ -16,18 +17,25 @@ MENTORS = [
         "nom": "L'arnacoeur",
         "emoji": "⚡",
         "desc": "Le pirate des échiquiers. Il vit pour l'arnaque, le bluff et la pendule.",
-        "vibe": "Chaos, psychologie et culot."
+        "vibe": "Chaos, psychologie et culot.",
+        "punchlines": {
+            "nom": "Ton nom de pirate ?",
+            "elo": "Donne ton Elo, on va braquer un GM ou un enfant au parc ",
+            "pgn": "Montre ta partie, que je vois où tu l'as bluffé",
+            "analyse": "Montre moi les arnaques"
+        }
     },
     {
         "id": "boa",
         "nom": "Le Boa Constrictor",
         "emoji": "🐍",
         "desc": "L'art de l'étouffement. Il déteste le risque et préfère supprimer tout espoir.",
-        "vibe": "Contrôle, prophylaxie et propreté."
+        "vibe": "Contrôle, prophylaxie et propreté",
         "punchlines": {
-            "nom": "Identité du joueur à évaluer ?",
-            "elo": "Précisez votre classement pour l'étalonnage technique.",
-            "pgn": "Soumettez le fichier PGN pour extraction des données."
+            "nom": "Tu es aussi silencieux que puissant, ton nom de code ?",
+            "elo": "Quelle est ton niveau de compréhension de l'art de l'étouffement positionnel ?",
+            "pgn": "Montre moi, comment tu l'as asphixié",
+            "analyse": "Analysons cette asphixie"
         }
     },
         {
@@ -35,11 +43,12 @@ MENTORS = [
         "nom": "Le Maître Zen",
         "emoji": "🧘",
         "desc": "L'équilibre avant tout. Il t'apprend la résilience et le calme dans le chaos.",
-        "vibe": "Paix, harmonie et acceptation."
+        "vibe": "Paix, harmonie et acceptation.",
         "punchlines": {
-            "nom": "Identité du joueur à évaluer ?",
-            "elo": "Précisez votre classement pour l'étalonnage technique.",
-            "pgn": "Soumettez le fichier PGN pour extraction des données."
+            "nom": "Sous quel nom ton esprit voyage-t-il sur l'échiquier ?",
+            "elo": "Quel chiffre le monde a-t-il posé sur ton niveau ? ",
+            "pgn": "Montre moi ce voyage, ton esprit est-il resté inénbranlable face au chaos de la bataille ?",
+            "analyse": "Guide mon esprit"
         }
     }
 ]
